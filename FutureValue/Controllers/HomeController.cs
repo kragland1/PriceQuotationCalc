70a19eaff5586d7c1cc1d8e39973253d62a9d7f7
kelec <kelec@LAPTOP-JGFOV90A>
2023-04-02 15:25:31 -0400

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotation.Models;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.D = 0;
+            ViewBag.T = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.D = model.CalculateDiscount();
+                ViewBag.T = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.D = 0;
+                ViewBag.T = 0;
+            }
+             return View(model);
+        }
+    }
+}
---
+++ FutureValue/Models/PriceQuotationModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a sales price.")]
+        [Range(1, 500, ErrorMessage =
+               "Sales price must be greater than 0 and less  500")]
+        public decimal? SalesPrice { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent")]
+        [Range(0.1, 100.0, ErrorMessage =
+               "Discount Percent must be between 0 and 100")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal? CalculateDiscount()
+        {
+            decimal? discountAmount = SalesPrice * (DiscountPercent / 100.0m);
+            return discountAmount;
+        }
+        public decimal? CalculateTotal()
+        {
+            return SalesPrice - CalculateDiscount();
+        }
+    }
+}
